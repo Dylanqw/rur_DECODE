8dd61a6fa28d569190cf58127ab89a33dcc32b76
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-10-22 14:47:40 -0400

    Update tankDriveTutorial.java

--- TeamCode/src/main/java/tankDriveTutorial.java
+++ TeamCode/src/main/java/tankDriveTutorial.java
@@ -1,9 +1,16 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class tankDriveTutorial extends OpMode {
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
     @Override
     public void init() {
-
+        leftFront = hardwareMap.get(DcMotor.class,"leftFront");
     }
 
     @Override
