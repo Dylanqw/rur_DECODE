253cf8b1b10f443bba2ca58f15e57d7c4bf0387c
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-10-23 18:38:15 -0400

    Update tankDriveTutorial.java

--- TeamCode/src/main/java/tankDriveTutorial.java
+++ TeamCode/src/main/java/tankDriveTutorial.java
@@ -1,5 +1,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class tankDriveTutorial extends OpMode {
 
@@ -8,9 +10,23 @@ public class tankDriveTutorial extends OpMode {
     DcMotor leftBack;
     DcMotor rightBack;
 
+
     @Override
     public void init() {
         leftFront = hardwareMap.get(DcMotor.class,"leftFront");
+        rightFront = hardwareMap.get(DcMotor.class,"rightFront");
+        leftBack = hardwareMap.get(DcMotor.class,"leftBack");
+        rightBack = hardwareMap.get(DcMotor.class,"rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
